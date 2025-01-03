5d3540339a019e831572df96b72bc967cdecc9c1
Diego Guido <dguidor@gmail.com>
2025-01-03 11:55:44 -0600

    Initial Commit

---
+++ PizzeriaApp-SwiftUIEssentialsTraining/PizzeriaApp_SwiftUIEssentialsTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzeriaApp_SwiftUIEssentialsTrainingApp.swift
+//  PizzeriaApp-SwiftUIEssentialsTraining
+//
+//  Created by Diego Guido on 03/01/25.
+//
+
+import SwiftUI
+
+@main
+struct PizzeriaApp_SwiftUIEssentialsTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
